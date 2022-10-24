d9a2a7cf16c2db8e22d784ae6a3caa857f9e843d
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-10-24 13:50:50 -0500

    Creación de variables

---
+++ Variables.cs
@@ -0,0 +1,28 @@
+// Tipos de variables que se pueden utilizar en C#
+
+using System;
+
+namespace Variables 
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int myNum = 5;
+      double myDouble = 2.99D;
+      string myString = "Hello";
+      char myChar = "D";
+      bool myBool= true;
+      
+      
+      Console.WriteLine(myNum);
+      Console.WriteLine(myDouble);
+      Console.WriteLine(myString);
+      Console.WriteLine(myChar);
+      Console.WriteLine(myBool);
+      }
+   }
+}
+      
+      
+  
