6bd5b2de429b94fb5dffe2c55c0269ba4f61ba2f
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-11-07 09:16:20 -0600

    Condicion if

---
+++ If.cs
@@ -0,0 +1,18 @@
+// Condicion if 
+
+using System;
+namespace MyCondicion 
+{
+  class Program
+  {
+  static void Main(string[] args)
+  {
+    int x = 100
+    int y = 65
+    if (y < x)
+    {
+      Console.WriteLine(" Y es menor a X ")
+      }
+     }
+   }    
+ }
