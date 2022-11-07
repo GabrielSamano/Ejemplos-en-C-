b8edf6797c933c0685bed12f31e400e6fed50407
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-11-07 09:32:02 -0600

    Condicion if y else

---
+++ If&else.cs
@@ -0,0 +1,28 @@
+// Condiciones if y else 
+
+using System;
+
+namespace MyCondicion 
+{
+  class Program
+  {
+    static void Main(String[] args)
+      {
+        int time = 18
+        if (time < 15)
+        {
+          Console.WriteLine("Good Afternoon");
+          }
+          else if (time < 10)
+          {
+          COnsole.WrtieLine("Good Morning");
+          }
+          else 
+          {
+          Console.WriteLine("Good Evening");
+          }
+       }
+    }
+  }
+          
+    
