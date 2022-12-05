c9619cb19e852678d3894f47dde1957ddaba67df
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-12-05 09:59:16 -0600

    Simple condición if & else

---
+++ ConditionIf&else.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace MYprogram
+{
+  class FirstCondition
+  {
+    static void Main(String[] args)
+    {
+      int dayTime = 18;
+      if (dayTime < 15)
+      {
+        Console.WriteLine("Buenas tardes");
+      }
+      else if (dayTime < 10)
+      {
+        Console.WriteLine("Buen dia");
+      }
+      else
+      {
+        Console.WriteLine("Buena Noche");
+      }
+    }
+  }
+}
+      
+      
+        
+        
