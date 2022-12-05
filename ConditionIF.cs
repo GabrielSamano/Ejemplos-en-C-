d33acc94b5f3c3a2181125d17c74233a6c4d4912
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-12-05 09:50:13 -0600

    Simple condición if

---
+++ ConditionIF.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace MyCondition
+{
+  class program
+  {
+    static void Main(String[] args)
+    {
+    int x = 15;
+    int y = 20;
+    if (y > x) 
+    {
+      Console.WriteLine(" Y es mas grande que X");
+      }
+    }
+  }
+}
+  
