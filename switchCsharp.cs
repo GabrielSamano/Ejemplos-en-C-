53ef2a67a58dd5114a04512ca9c5b8d0800f3d07
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-12-07 09:14:44 -0600

    Loop switch

---
+++ switchCsharp.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace MyProyect
+{
+  class Proyect
+    {
+    static void Main(String[] args)
+      {
+      int day = 5;
+      switch(day)
+        {
+          case 1:
+            Console.WriteLine("Monday");
+            break;
+          case 2:
+            Console.WriteLine("Tuesday");
+            break;
+          case 3:
+            Console.WriteLine("Wednesday");
+            break;
+          case 4:
+            Console.WriteLine("Thursday");
+            break;
+          case 5:
+            Console.WriteLine("Friday");
+            break;
+          case 6:
+            Console.WriteLine("Saturday");
+            break;
+          case 7:
+            Console.WriteLine("Sunday");
+            break;
+            
+        }
+      }
+    }
+  }
+            
+          
+      
