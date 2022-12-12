7afc212e0edef79e1d1e937aed8399caf7d607d1
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-12-12 08:59:27 -0600

    Create Metodos.cs

---
+++ Metodos.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Metodo
+{
+  class Aplicacion
+  {
+    static void MyMethod()
+    {
+      Console.WriteLine("Llamando al metodo");
+    }
+
+    static void Main(string[] args)
+    {
+      MyMethod();
+    }
+  }
+}
